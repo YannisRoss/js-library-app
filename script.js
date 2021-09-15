921dd77507c39ee9867d281b4675dd07707d303d
Grimhamr <rossiadisj@gmail.com>
2021-09-15 18:58:54 +0300

    add isRead changable status

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let myLibrary = []
 function Book(title, author) {
     this.title = title
     this.author = author
-    this.read = false
+    this.isRead = false
 }
 
 function addBookToLibrary(book) {
@@ -30,7 +30,6 @@ function showBooks() {
     return booksString
 
 }
-
 
 
 document.getElementById("new-book-button").addEventListener("click",
@@ -61,8 +60,9 @@ function showBooksClick() {
     while (i < myLibrary.length) {
 
         bookDiv = document.createElement("div")
-        bookDiv.setAttribute("class", "book-div")
-        bookDiv.setAttribute("id", "book-div" + i)
+            bookDiv.setAttribute("class", "book-div")
+            bookDiv.setAttribute("id", "book-div" + i)
+            booksContainer.appendChild(bookDiv)
 
         bookDiv.innerHTML = myLibrary[i].title + ", by " + myLibrary[i].author
         deleteBookButton = document.createElement("button")
@@ -85,11 +85,28 @@ function showBooksClick() {
 
         readCheckbox = document.createElement("input")
             readCheckbox.setAttribute("type", "checkbox")
+            readCheckbox.setAttribute("id", i +"-checkbox")
             bookDiv.appendChild(readCheckbox)
 
+            if (myLibrary[i].isRead) {
+                document.getElementById(i + "-checkbox").checked = true;
+            }
+            else {
+                console.log(myLibrary[i].title + 'is not read')
+                document.getElementById(i + "-checkbox").checked = false;
+                }
 
+            readCheckbox.addEventListener('change', function() {
+                if (this.checked) {
+                    myLibrary[this.id[0]].isRead = true
+                    console.log(myLibrary[this.id[0]].title + " is read: " + myLibrary[this.id[0]].isRead)
+                } else {
+                    myLibrary[this.id[0]].isRead = false
+                    console.log(myLibrary[this.id[0]].title + " is read: " + myLibrary[this.id[0]].isRead)
 
-        booksContainer.appendChild(bookDiv)
+                }
+                })
+
 
 
         i++
